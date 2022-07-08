ec58793c9b1b9fb920de0bade91f15b09a49eda7
Nicholas Singh <nicholas1singh@gmail.com>
2022-07-08 17:17:20 -0400

    added page title

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const desiredChainId = ChainId.Goerli;
   return (
     <ThirdwebProvider desiredChainId={desiredChainId}>
       <ChakraProvider>
+        <Head>
+          <title>Twitter NFT Generator</title>
+        </Head>
         <Component {...pageProps} />
       </ChakraProvider>
     </ThirdwebProvider>
